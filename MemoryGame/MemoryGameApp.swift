7ecdeb2d70c9371493ef577a1d2d5f7bbc9c97e3
Isis Silva <isis.silva@arctouch.com>
2022-11-26 16:07:03 -0300

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Isis Silva on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
