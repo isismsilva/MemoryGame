e938d424474701bd3380230442ce10638ea67d5f
Isis Silva <isisms2602@gmail.com>
2022-12-04 21:55:52 -0300

    separated responsibilities

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -10,14 +10,12 @@ import SwiftUI
 struct ContentView: View {
   @ObservedObject var game: EmojiMemoryGame
   @Namespace var dealingNamespace
-  @State var dealt = Set<Int>()
   
   var body: some View {
     ZStack(alignment: .bottom) {
       VStack {
         gameBody
         deckBody
-        
         HStack {
           shuffleButton
           Spacer()
@@ -33,14 +31,14 @@ struct ContentView: View {
 private extension ContentView {
   var gameBody: some View {
     AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-      if isUndealt(card) || (card.isMatched && !card.isFaceUp) {
+      if game.isUndealt(card) || (card.isMatched && !card.isFaceUp) {
         Color.clear
       } else {
         CardView(card: card)
           .matchedGeometryEffect(id: card.id, in: dealingNamespace)
           .padding(4)
           .transition(.asymmetric(insertion: .identity, removal: .opacity))
-          .zIndex(zIndex(of: card))
+          .zIndex(game.zIndex(of: card))
           .onTapGesture {
             withAnimation {
               game.choose(card: card)
@@ -52,19 +50,19 @@ private extension ContentView {
   
   var deckBody: some View {
     ZStack {
-      ForEach(game.cards.filter(isUndealt)) { card in
+      ForEach(game.cards.filter(game.isUndealt)) { card in
         CardView(card: card)
           .matchedGeometryEffect(id: card.id, in: dealingNamespace)
           .transition(.asymmetric(insertion: .opacity, removal: .identity))
-          .zIndex(zIndex(of: card))
+          .zIndex(game.zIndex(of: card))
       }
     }
     .frame(width: DrawingConstants.undealWidth, height: DrawingConstants.undealHeight)
     .foregroundColor(.orange)
     .onTapGesture {
       for card in game.cards {
-        withAnimation(dealAnimation(for: card)) {
-          deal(card)
+        withAnimation(game.dealAnimation(for: card)) {
+          game.deal(card)
         }
       }
     }
@@ -81,34 +79,12 @@ private extension ContentView {
   var restartButton: some View {
     RoundedButton(title: "Restart") {
       withAnimation {
-        dealt = []
         game.restart()
       }
     }
   }
   
-  func deal(_ card: MemoryGame<String>.Card) {
-    dealt.insert(card.id)
-  }
-  
-  func isUndealt(_ card: MemoryGame<String>.Card) -> Bool {
-    !dealt.contains(card.id)
-  }
-  
-  func dealAnimation(for card: EmojiMemoryGame.Card) -> Animation {
-    var delay = 0.0
-    if let index = game.cards.firstIndex(where: { $0.id == card.id }) {
-      delay = Double(index) * (DrawingConstants.animationDuration / Double(game.cards.count))
-    }
-    return .easeInOut(duration: DrawingConstants.animationDuration).delay(delay)
-  }
-  
-  func zIndex(of card: EmojiMemoryGame.Card) -> Double {
-    -Double(game.cards.firstIndex(where: { $0.id == card.id }) ?? 0)
-  }
-  
   enum DrawingConstants {
-    static let animationDuration: Double = 1
     static let undealWidth: Double = 68
     static let undealHeight: Double = 100
   }
--- MemoryGame/EmojiMemoryGame.swift
+++ MemoryGame/EmojiMemoryGame.swift
@@ -5,10 +5,11 @@
 //  Created by Isis Silva on 11/27/22.
 //
 
-import Foundation
+import SwiftUI
 
 final class EmojiMemoryGame: ObservableObject {
   @Published private var model = createMemoryGame()
+  @Published var dealt = Set<Int>()
   
   typealias Card = MemoryGame<String>.Card
   
@@ -17,7 +18,7 @@ final class EmojiMemoryGame: ObservableObject {
   private static func createMemoryGame() -> MemoryGame<String> {
     MemoryGame<String>(numberOfPairsOfCards: 8) { emojis[$0] }
   }
- 
+  
   var cards: Array<Card> {
     model.cards
   }
@@ -31,8 +32,28 @@ final class EmojiMemoryGame: ObservableObject {
   }
   
   func restart() {
+    dealt = []
     model = EmojiMemoryGame.createMemoryGame()
   }
   
+  func deal(_ card: MemoryGame<String>.Card) {
+    dealt.insert(card.id)
+  }
+  
+  func isUndealt(_ card: MemoryGame<String>.Card) -> Bool {
+    !dealt.contains(card.id)
+  }
+  
+  func dealAnimation(for card: EmojiMemoryGame.Card) -> Animation {
+    var delay = 0.0
+    if let index = cards.firstIndex(where: { $0.id == card.id }) {
+      delay = Double(index) * (1 / Double(cards.count))
+    }
+    return .easeInOut(duration: 1).delay(delay)
+  }
+  
+  func zIndex(of card: Card) -> Double {
+    -Double(cards.firstIndex(where: { $0.id == card.id }) ?? 0)
+  }
 }
 
